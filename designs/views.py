803538d304e2780dd9807d0e92aa8cb5485eaf3c
Justin859 <justinhammond859@yahoo.com>
2017-05-19 22:23:09 +0200

    Database issue fix

--- designs/views.py
+++ designs/views.py
@@ -4,21 +4,24 @@ from django.http import HttpResponse
 from .models import Birth_Stones, Premier_Brands
 # Create your views here.
 
-#Global Variables
-birthstones = Birth_Stones.objects.order_by('stone_month')
-premier_brands = Premier_Brands.objects.order_by('brand_name')
-
-
 def index(request):
+    birthstones = Birth_Stones.objects.order_by('stone_month')
+    premier_brands = Premier_Brands.objects.order_by('brand_name')
     return render(request, 'index.html', {'birthstones': birthstones, 'premier_brands': premier_brands})
 
 def about(request):
+    birthstones = Birth_Stones.objects.order_by('stone_month')
+    premier_brands = Premier_Brands.objects.order_by('brand_name')
     return render(request, 'details/about.html', {'birthstones': birthstones, 'premier_brands': premier_brands})
 
 def contact(request):
+    birthstones = Birth_Stones.objects.order_by('stone_month')
+    premier_brands = Premier_Brands.objects.order_by('brand_name')
     return render(request, 'details/contact.html', {'birthstones': birthstones, 'premier_brands': premier_brands})    
 
 def detail(request, premier_brand_name):
+    birthstones = Birth_Stones.objects.order_by('stone_month')
+    premier_brands = Premier_Brands.objects.order_by('brand_name')
     brand = get_object_or_404(Premier_Brands, pk=premier_brand_name)
     return render(request, 'brands/detail.html', {'brand': brand, 'birthstones': birthstones, 'premier_brands': premier_brands})
 
