387a6201b46c7838cf2f202b8fe527f57f67f59c
Justin859 <justinhammond859@yahoo.com>
2017-05-31 18:11:36 +0200

    Hide keys

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 import dj_database_url
 
-from secrets import *
+from secret_keys import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
